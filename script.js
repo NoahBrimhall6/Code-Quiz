cf41586570a0a8fa056952fb0875a017f427cc88
Noah Brimhall <noahbrimhall6@gmail.com>
2022-08-10 18:26:46 -0600

    Added highscore system to script.js

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 var startBtn = document.querySelector("#start-btn");
 var page = document.querySelectorAll(".page");
 var questions = document.querySelectorAll(".questions");
+var score = document.querySelector("#score");
+var form = document.querySelector("form");
+var input = document.querySelector("#input");
 
 var timeLeft = 100;
 var pageNumber = 0;
+var highScores = [];
 
 function renderPage() {
     pageNumber++;
     page[pageNumber-1].style.display = "none";
     page[pageNumber].style.display = "flex";
+
+    if (pageNumber === 6) {
+        score.textContent = timeLeft;
+    }
 
     console.log("page number is " + pageNumber);
 };
@@ -26,11 +34,23 @@ function answerPicked(event) {
     }  
 }
 
+function submit(event) {
+    event.preventDefault();
+    inputText = input.value.trim();
+    if (inputText === "") {return;}
+
+    highScores.push(inputText);
+    input.value = "";
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    renderPage();
+}
+
 function runQuiz() {
     renderPage();
     for (var i = 0; i < questions.length; i++){
         questions[i].addEventListener("click", answerPicked);
     }
+    form.addEventListener("submit", submit);
 }
 
 startBtn.addEventListener("click", runQuiz);
